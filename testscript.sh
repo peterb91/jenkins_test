9ab0cddf3cc7b7dfcc52aee6198be5d2de655d2f
Piotr Bednarz <pbednarz@akamai.com>
2019-02-28 13:20:44 +0100

    push

--- testscript.sh
+++ testscript.sh
@@ -3,5 +3,6 @@ echo "This script is from GitHub"
 NUM=$(($NUM1 + $NUM2))
 echo "Result of the sum:"
 echo "$NUM"
+echo "FGFFGFDGFDGGDG"
 echo "**************************"
 echo "This was run automatically!"
